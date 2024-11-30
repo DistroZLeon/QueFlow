2905212dee26a6a8d4fca7a3856eebd6ecfa7900
Archerraikou <nedelcu.danut6@gmail.com>
2024-11-30 21:05:14 +0200

    Am adaugat modelele

---
+++ QueFlow/Models/Answer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QueFlow.Models
+{
+    public class Answer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Continutul raspunsului este obligatoriu")]
+        public string Text { get; set; }
+        public DateTime DatePosted { get; set; }
+        public int? QuestionId {  get; set; }
+        public virtual Question? Q { get; set; }
+        public string? UserId { get; set; } 
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
---
+++ QueFlow/Models/Question.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace QueFlow.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Titlul este obligatoriu")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Continutul este obligatoriu")]
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Category este obligatoriu")]
+        public int? CategoryId { get; set; }
+        public virtual Category? Category { get; set; }
+        public string? UserId { get; set; }
+        public virtual ApplicationUser? User { get; set; }
+        public virtual ICollection<Answer>? Answers { get; set; }
+        public IEnumerable<SelectListItem>? Categ { get; set; } 
+    }
+}
