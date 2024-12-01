4c1d504efd1b3fbcf24cc6ff40e8e50174011cd3
distrozleon <alystinga@yahoo.ro>
2024-12-01 23:30:28 +0200

    Ai dat commit

--- QueFlow/Models/Category.cs
+++ QueFlow/Models/Category.cs
@@ -8,7 +8,10 @@ namespace QueFlow.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage ="Numele categoriei este obligatoriu")]
+        [StringLength(100,ErrorMessage ="Dimensiunea maxima este de 100 caractere")]
+        [MinLength(5,ErrorMessage ="Dimensiunea minima de 5 caractere")]
         public string Name { get; set; }
+        [StringLength(300,ErrorMessage ="Exista o limita de caractere :)")]
         public string? Description { get; set; }
         public virtual ICollection<Question>? Questions { get; set; }
     }
