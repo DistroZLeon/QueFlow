4fd3e1e598fa368707b3b9cdc7e6a6e571c37174
Archerraikou <nedelcu.danut6@gmail.com>
2024-12-13 15:04:07 +0200

    Implementat coloanele din ApplicationUser

--- QueFlow/Data/Migrations/20241213125751_QueFlow-8.Designer.cs
+++ QueFlow/Data/Migrations/20241213125751_QueFlow-8.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QueFlow.Data;
 
@@ -11,9 +12,11 @@ using QueFlow.Data;
 namespace QueFlow.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241213125751_QueFlow-8")]
+    partial class QueFlow8
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- QueFlow/Models/ApplicationUser.cs
+++ QueFlow/Models/ApplicationUser.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QueFlow.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? Nume {  get; set; }
+        public string? ProfPic {  get; set; }
+        public string? Desc {  get; set; }
+        public virtual ICollection<Question>? Questions { get; set; }
+        public virtual ICollection<Answer>? Answers { get; set; }
+        [NotMapped]
+        public IEnumerable<SelectListItem>? AllRoles { get; set; }
     }
 }
